221b08327e56007c439fa3b34ccc308c23eab258
Ryan Ho <ryanho@Ryans-MacBook-Pro.local>
2020-05-18 06:41:13 -1000

    ch11

--- server.js
+++ server.js
@@ -60,6 +60,4 @@ var server = http.createServer(function (req, res) {
   }
   res.end();
 });
-server.listen(3000,function(){
-  console.log("server is listening on 3000");
-});
+server.listen(3000);
