a352114a88dca4088fa5b5fb03cbf78f533bfe97
Ryan Ho <ryanho@Ryans-MacBook-Pro.local>
2020-05-17 21:59:17 -1000

    ch5

--- server.js
+++ server.js
@@ -9,52 +9,48 @@ var http= require('http');
 var path= require('path');
 
 var indexJs= fs.readFileSync("./public/index.js",'utf8');
-console.log("==Read indexJs");
+console.log(indexJs);
 var style= fs.readFileSync("./public/style.css",'utf8');
-console.log("==Read style");
+console.log(style);
 
 var indexHtml= fs.readFileSync("./public/index.html",'utf8');
-console.log("==Read indexHtml");
+console.log(indexHtml);
 var fourZero= fs.readFileSync("./public/404.html",'utf8');
-console.log("==read 404");
+console.log(fourZero);
+
 
 var server = http.createServer(function (req, res) {
+
   console.log("== Got a request");
   console.log("  -- HTTP method:", req.method);
   console.log("  -- Resource:", req.url);
   console.log("  -- Headers:", req.headers);
 
-  if(req.url=="/style.css"){
+  if(req.url==="/style.css"){
     res.writeHead(200, {
       "Content-Type": "text/css"
     });
     res.write(style);
   }
-  if(req.url=="/index.js"){
+  if(req.url==="/index.js"){
     res.writeHead(200, {
       "Content-Type": "application/javascript"
     });
     res.write(indexJs);
   }
-  if(req.url=="/index.html"){
+  if(req.url==="/index.html"||req.url==="/"){
     res.writeHead(200, {
       "Content-Type": "text/html"
     });
     res.write(indexHtml);
   }
-  if(req.url=="/"){
-    res.writeHead(200, {
-      "Content-Type": "text/html"
-    });
-      res.write(indexHtml);
-  }
-  if(req.url=="/404.html"){
+  if(req.url==="/404.html"){
     res.writeHead(200, {
       "Content-Type": "text/html"
     });
     res.write(fourZero);
   }
-  else{
+  if(req.url!=="404.html"&&req.url!=="/index.html"&&req.url!=="/"&&req.url!=="/style.css"&&req.url!=="/index.js"){
     res.writeHead(404, {
       "Content-Type": "text/html"
     });
